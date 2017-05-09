53cb8353137486c8c89b3165c91a218fafba21ed
Kurt <kurtanderson234@gmail.com>
2017-05-09 16:09:06 -0500

    Updated Class and Main File

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,3 +1,8 @@
 import media
 
-toy_story = 
+pulp_fiction = media.Movie("Pulp Fiction",
+                           "The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+                           r"https://images-na.ssl-images-amazon.com/images/M/MV5BMTkxMTA5OTAzMl5BMl5BanBnXkFtZTgwNjA5MDc3NjE@._V1_SY1000_CR0,0,673,1000_AL_.jpg",
+                           r"http://www.imdb.com/title/tt0110912/videoplayer/vi2620371481?ref_=tt_ov_vi"
+                           )
+print(pulp_fiction.storyline)
